37620a6200eb55a01a285845392b69284e69ba09
viniciusmatheussesc <vmpires@sescto.com.br>
2023-06-08 07:41:14 -0300

    commit inicial

---
+++ flutter-notifications-start/lib/http/web.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:meetups/models/event.dart';
+import 'package:http/http.dart' as http;
+
+const String baseUrl = 'http://localhost:8080/api';
+
+Future<List<Event>> getAllEvents() async {
+  final response = await http.get(Uri.parse('$baseUrl/events'));
+
+  if (response.statusCode == 200) {
+    final List<dynamic> decodedJson = jsonDecode(response.body);
+    return decodedJson.map((dynamic json) => Event.fromJson(json)).toList();
+  } else {
+    throw Exception('Falha ao carregar os eventos');
+  }
+}
