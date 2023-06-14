00afee8dd03dfd6b64fa594bb1d90e4d8ee0cfb4
viniciusmatheusbsesc <vmpires@sescto.com.br>
2023-06-14 15:28:25 -0300

    ..

--- flutter_notifications_start/lib/http/web.dart
+++ flutter_notifications_start/lib/http/web.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+import 'package:flutter_notifications_start/models/device.dart';
 import 'package:flutter_notifications_start/models/event.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
-const String baseUrl = 'http://192.168.100.198:8080/api';
+const String baseUrl = 'http://10.63.10.115:8080/api';
 
 Future<List<Event>> getAllEvents() async {
   final response = await http.get(Uri.parse('$baseUrl/events'));
@@ -12,5 +14,29 @@ Future<List<Event>> getAllEvents() async {
     return decodedJson.map((dynamic json) => Event.fromJson(json)).toList();
   } else {
     throw Exception('Falha ao carregar os eventos');
+  }
+}
+
+void sendDevice(Device device) async {
+  final response = await http.post(
+    Uri.parse('$baseUrl/devices'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8'
+    },
+    body: jsonEncode(<String, String>{
+      'token': device.token ?? '',
+      'modelo': device.model ?? '',
+      'marca': device.brand ?? ''
+    }),
+  );
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setString('pushToken', device.token!);
+  prefs.setBool('tokenSent', false);
+
+  if (response.statusCode != 200) {
+    throw Exception('Falha ao criar o dispositivo');
+  } else {
+    prefs.setBool('tokenSent', true);
   }
 }
--- flutter_notifications_start/lib/main.dart
+++ flutter_notifications_start/lib/main.dart
@@ -1,7 +1,91 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_notifications_start/http/web.dart';
+import 'package:flutter_notifications_start/models/device.dart';
 import 'package:flutter_notifications_start/screens/events_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: true,
+    badge: true,
+    carPlay: true,
+    criticalAlert: true,
+    provisional: true,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('Permissão concedida pelo usuario: ${settings.authorizationStatus}');
+    _startPushnotificationHandler(messaging);
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    print(
+        'Permissão concedida provisoriamente pelo usuario: ${settings.authorizationStatus}');
+    _startPushnotificationHandler(messaging);
+  } else {
+    print('Permissão negada pelo usuario.');
+  }
+
+  runApp(App());
+}
+
+void _startPushnotificationHandler(FirebaseMessaging messaging) async {
+  String? token = await messaging.getToken();
+  print('TOKEN: $token');
+  _setPushToken(token);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Recebi uma mensagem');
+    print('Dados da mensagem: ${message.data}');
+
+    if (message.notification != null) {
+      print(
+          'A mensagem contém uma notificação: ${message.notification!.title}, ${message.notification!.body}');
+    }
+  });
+}
+
+void _setPushToken(String? token) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? prefsToken = prefs.getString('pushToken');
+  bool? prefSent = prefs.getBool('tokenSent');
+  print('Prefs Token: $prefsToken');
+
+  if (prefsToken != token || (prefsToken == token && prefSent == false)) {
+    print('Enviando Token para o servidor!');
+
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    String? brand;
+    String? model;
+
+    if (Platform.isAndroid) {
+      AndroidDeviceInfo androidDevice = await deviceInfo.androidInfo;
+      print('Modelo: ${androidDevice.model}');
+      model = androidDevice.model;
+      brand = androidDevice.brand;
+    } else {
+      IosDeviceInfo iosDevice = await deviceInfo.iosInfo;
+      print('Modelo: ${iosDevice.utsname.machine}');
+      model = iosDevice.utsname.machine;
+      brand = 'Apple';
+    }
+
+    Device device = Device(brand: brand, model: model, token: token);
+
+    sendDevice(device);
+  } else {
+    print('aparelho cadastrado');
+  }
+}
 
 class App extends StatelessWidget {
   @override
